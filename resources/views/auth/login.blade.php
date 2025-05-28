89b7ddb1903910bf9067a42ffbc5f934298a7744
Suman Regmi <limitlessplaying@gmail.com>
2025-05-28 21:57:41 +0545

    Bootstrap to tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,64 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="container mx-auto px-4">
+    <div class="flex justify-center">
+        <div class="w-full max-w-2xl">
+            <div class="bg-white shadow rounded-lg">
+                <div class="bg-gray-100 px-6 py-3 text-lg font-semibold border-b">
+                    {{ __('Login') }}
+                </div>
 
-                <div class="card-body">
+                <div class="p-6">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="mb-4 flex flex-wrap items-center">
+                            <label for="email" class="w-full md:w-1/3 text-right pr-4 text-gray-700 font-medium">
+                                {{ __('Email Address') }}
+                            </label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="w-full md:w-2/3">
+                                <input id="email" type="email" class="w-full px-4 py-2 border rounded @error('email') border-red-500 @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="text-red-500 text-sm mt-1 block" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="mb-4 flex flex-wrap items-center">
+                            <label for="password" class="w-full md:w-1/3 text-right pr-4 text-gray-700 font-medium">
+                                {{ __('Password') }}
+                            </label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <div class="w-full md:w-2/3">
+                                <input id="password" type="password" class="w-full px-4 py-2 border rounded @error('password') border-red-500 @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="text-red-500 text-sm mt-1 block" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                        <div class="mb-4 flex flex-wrap">
+                            <div class="md:ml-auto md:w-2/3 md:pl-4">
+                                <label class="inline-flex items-center">
+                                    <input class="form-checkbox text-indigo-600" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <span class="ml-2 text-gray-700">{{ __('Remember Me') }}</span>
+                                </label>
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="ms-5 ps-5 pe-5 btn btn-primary">
+                        <div class="flex flex-wrap items-center">
+                            <div class="md:ml-auto md:w-2/3 md:pl-4">
+                                <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="ml-4 text-blue-600 hover:underline text-sm" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,232 +1,150 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <title>Share Insight Nepal</title>
-    <link rel="icon" href="{{asset('images/shareInsight.JPG')}}" type="image/jpg" sizes="32x32">
-    <style>
-      .liveLiveMarket {
-    background-image: url("{{asset('images/liveMarket1.png')}}");
-    background-size: cover;
-    background-repeat: no-repeat;
-    color: white;
-  }
-  .indexLiveMarket {
-    background-image: url("{{asset('images/index1.png')}}");
-    background-size: cover;
-    background-repeat: no-repeat;
-    color: white;
-  }
-  .text-4xl{
-    color: white;
-  }
-  .text-lg{
-    color: white;
-  }
-  .topLiveMarket {
-    background-image: url("{{asset('images/top1.png')}}");
-    background-size: cover;
-    background-repeat: no-repeat;
-    color: white;
-  }
-  .brokerLiveMarket {
-    background-image: url("{{asset('images/broker1.png')}}");
-    background-size: cover;
-    background-repeat: no-repeat;
-    color: white;
-  }
-  .ipoLiveMarket {
-    background-image: url("{{asset('images/ipo1.png')}}");
-    background-size: cover;
-    background-repeat: no-repeat;
-    color: white;
-  }
-  .ipoLiveMarket p{
-    color: white
-  }
-  .watchlistLiveMarket {
-    background-image: url("{{asset('images/watchlist1.png')}}");
-    background-size: cover;
-    background-repeat: no-repeat;
-    color: white;
-  }
-  .portfolioLiveMarket {
-    background-image: url("{{asset('images/portfolio1.png')}}");
-    background-size: cover;
-    background-repeat: no-repeat;
-    color: white;
-  }
-  .main video{
-  position: absolute;
-  top: 13.5%;
-  right: 7.6%;
-  width: 85%;
-  height: 37%;
-  object-fit: cover;
-}
-    </style>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+  <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+  <title>Share Insight Nepal</title>
+  <link rel="icon" href="{{asset('images/shareInsight.JPG')}}" type="image/jpg" sizes="32x32">
+  <style>
+    .liveLiveMarket, .indexLiveMarket, .topLiveMarket, .brokerLiveMarket, .ipoLiveMarket, .watchlistLiveMarket, .portfolioLiveMarket {
+      background-size: cover;
+      background-repeat: no-repeat;
+      color: white;
+    }
+    .liveLiveMarket { background-image: url("{{asset('images/liveMarket1.png')}}"); }
+    .indexLiveMarket { background-image: url("{{asset('images/index1.png')}}"); }
+    .topLiveMarket { background-image: url("{{asset('images/top1.png')}}"); }
+    .brokerLiveMarket { background-image: url("{{asset('images/broker1.png')}}"); }
+    .ipoLiveMarket { background-image: url("{{asset('images/ipo1.png')}}"); }
+    .watchlistLiveMarket { background-image: url("{{asset('images/watchlist1.png')}}"); }
+    .portfolioLiveMarket { background-image: url("{{asset('images/portfolio1.png')}}"); }
+    .text-4xl, .text-lg, .ipoLiveMarket p { color: white; }
+    .main video {
+      position: absolute;
+      top: 13.5%;
+      right: 7.6%;
+      width: 85%;
+      height: 37%;
+      object-fit: cover;
+    }
+  </style>
   <script>
-    // Add your custom JavaScript or jQuery code here
-    // Example animation using JavaScript
-    document.addEventListener("DOMContentLoaded", function() {
-      // const element = document.querySelector(".text-4xl");
-      // const element1 = document.querySelector(".text-lg");
+    document.addEventListener("DOMContentLoaded", function () {
       const element2 = document.querySelector(".textBox");
-      
-      // element.classList.add("animate-bounce");
       element2.classList.add("animate-bounce");
-      // element1.classList.add("animate-pulse");
-
     });
   </script>
 </head>
-<body class="container">
-    <section class="topHeader row"> 
-        <div class="logo col-xl-9 col-lg-7 col-md-7 col-sm-7 col-6 me-auto my-auto">
-          <div class="d-flex gap-1">
-            <a class="my-auto" href="{{ url('/') }}"><img class="img-fluid" src="{{asset('images/shareInsight1.JPG')}}"alt="Company Logo" width="150px"></a>
-            <a class="my-auto" href="{{ url('/') }}"><img class="img-fluid" src="{{asset('images/shareInsight2.png')}}"alt="Company Banner" width="300px" height="40px"></a>
+<body class="container mx-auto">
+  <section class="topHeader grid grid-cols-12 items-center gap-4 py-4">
+    <div class="logo col-span-12 lg:col-span-9 flex gap-2">
+      <a href="{{ url('/') }}"><img class="w-36" src="{{asset('images/shareInsight1.JPG')}}" alt="Company Logo" /></a>
+      <a href="{{ url('/') }}"><img class="w-72 h-10" src="{{asset('images/shareInsight2.png')}}" alt="Company Banner" /></a>
+    </div>
+    <div class="col-span-12 lg:col-span-3 flex flex-col sm:flex-row gap-2 justify-center items-center">
+      <a class="text-white" href="/home"><button class="bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 rounded">Dashboard</button></a>
+      @guest
+        <a class="text-white" href="/loginRegister"><button class="bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 rounded">Login/Register</button></a>
+      @else
+        <div class="flex items-center gap-2">
+          <img src="{{ Auth::user()->profileImage }}" alt="Profile Image" class="w-10 h-10 rounded-full"/>
+          <div class="relative">
+            <a id="navbarDropdown" class="cursor-pointer text-black" href="/profile" role="button">
+              ▼
+            </a>
+            <div class="absolute mt-1 bg-white rounded shadow w-40 p-2 hidden group-hover:block">
+              <a href="/profile" class="block bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 mb-1 rounded text-center">{{ Auth::user()->firstName }} {{ Auth::user()->lastName }}</a>
+              <a href="/logout" class="block bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded text-center"
+                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+              </a>
+              <form id="logout-form" action="/logout" method="POST" class="hidden">@csrf</form>
+            </div>
           </div>
         </div>
-        <div class="col-xl-2 col-lg-2 col-md-4 col-sm-3 col-3 my-3 mx-auto">
-          <div class="row">
-            <a class="col-xl-5 col-lg-3 col-md-5 col-sm-2 col-2 dashboard nav-link active pe-1 pt-1 my-auto me-auto" aria-current="page" href="/home"><button class="btn btn-primary">Dashboard</button></a>
-            @guest
-              <a class="col-xl-6 col-lg-4 col-md-6 col-sm-3 col-3 login nav-link active ps-1 pe-1 my-auto pt-1" aria-current="page" href="/loginRegister"><button class="btn btn-primary">Login/Register</button></a>
-            @else
-              <div class="col-xl-6 col-lg-4 col-md-6 col-sm-3 col-3">
-                <div class="d-flex gap-1">
-                <img src="{{ Auth::user()->profileImage }}" alt="Profile Image" class="w-12 img-fluid rounded-circle">
-                <div class="nav-item dropdown mt-auto ">
-                  <a id="navbarDropdown" class="nav-link dropdown-toggle text-black" href="/profile" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                  </a>
+      @endguest
+    </div>
+  </section>
 
-                  <div class="dropdown-menu dropdown-menu-end p-1" aria-labelledby="navbarDropdown">
-                    <a href="/profile" class="btn btn-primary w-full mb-1"> {{ Auth::user()->firstName }}  {{ Auth::user()->lastName }} </a>
-                      <a class="btn btn-danger w-full" href="/logout"
-                         onclick="event.preventDefault();
-                                       document.getElementById('logout-form').submit();">
-                          {{ __('Logout') }}
-                      </a>
-                      <form id="logout-form" action="/logout" method="POST" class="d-none">
-                          @csrf
-                      </form>
-                  </div>
-                </div>
-                </div>
-              </div>
-            @endguest
-          </div>
-        </div>
-    </section>
-    <p class="border border-4"></p>
+  <hr class="border-4"/>
 
-  <!-- video Section -->
-  <section class="bg-white-100 col-12 py-20 main rounded">
-    <div class="container mx-auto">
-      <div class="text-center">
-        <video autoplay muted loop id="video-bg" class="border border-5 border-secondary rounded">
-          <source src="{{asset('images/bg.mp4')}}" type="video/mp4">
-          Your browser does not support the video tag.
-        </video>   
-        <div class="textBox">
+  <section class="bg-white py-20 main rounded relative">
+    <div class="text-center relative z-10">
+      <video autoplay muted loop id="video-bg" class="border-4 border-gray-400 rounded mx-auto">
+        <source src="{{asset('images/bg.mp4')}}" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div class="textBox mt-6">
         <h1 class="text-4xl font-bold">Welcome to Share Insight Nepal</h1>
         <p class="mt-4 m-2 text-lg">Your Ultimate Destination for Nepal Stocks Market Data</p>
       </div>
+    </div>
+  </section>
+
+  <section class="py-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div class="liveLiveMarket p-6 rounded shadow">
+        <h2 class="text-2xl font-bold mb-4">Live Market</h2>
+        <p>Get up-to-date information on stock prices, trading volume, and market trends.</p>
+      </div>
+      <div class="indexLiveMarket p-6 rounded shadow">
+        <h2 class="text-2xl font-bold mb-4">Live Market Index</h2>
+        <p>Stay updated with the performance of various market indices and sub-indices.</p>
+      </div>
+      <div class="topLiveMarket p-6 rounded shadow">
+        <h2 class="text-2xl font-bold mb-4">Top Gainers/Losers</h2>
+        <p>Stay informed about the stocks that are performing well and those that are experiencing losses.</p>
+      </div>
+      <div class="watchlistLiveMarket p-6 rounded shadow">
+        <h2 class="text-2xl font-bold mb-4">Watchlist</h2>
+        <p>Create and maintain a personalized watchlist of stocks you want to monitor closely.</p>
+      </div>
+      <div class="portfolioLiveMarket p-6 rounded shadow">
+        <h2 class="text-2xl font-bold mb-4">Portfolio</h2>
+        <p>Track your investment holdings and view your portfolio performance.</p>
+      </div>
+      <div class="ipoLiveMarket p-6 rounded shadow">
+        <h2 class="text-2xl font-bold mb-4">IPO Results</h2>
+        <p class="text-gray-600 mb-6">Stay updated with IPO results.</p>
+        <p>Stay informed about the latest initial public offerings and their results.</p>
+      </div>
+      <div class="brokerLiveMarket p-6 rounded shadow">
+        <h2 class="text-2xl font-bold mb-4">Broker Lists</h2>
+        <p>Find info about authorized stockbrokers and their services.</p>
       </div>
     </div>
   </section>
 
-  <!-- Features Section -->
-  <section class="py-6 row">
-    <div class="container mx-auto">
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-        <!-- Live Market -->
-        <div class="liveLiveMarket bg-white shadow p-6 rounded-lg">
-          <h2 class="text-2xl font-bold mb-4">Live Market</h2>
-          <!-- Add your live market widget here -->
-          <p>Get up-to-date information on stock prices, trading volume, and market trends.</p>
-        </div>
-
-        <!-- Live Market Index Section -->
-        <div class="bg-white indexLiveMarket shadow p-6 rounded-lg">
-          <h2 class="text-2xl font-bold mb-4">Live Market Index</h2>
-          <!-- Add your live market index and sub-indices content here -->
-          <p>Stay updated with the performance of various market indices and sub-indices, such as the stock exchange index, sector-specific indices, and more.</p>
-        </div>
-
-        <!-- Top Gainers/Losers -->
-        <div class="topLiveMarket bg-white shadow p-6 rounded-lg">
-          <h2 class="text-2xl font-bold mb-4">Top Gainers/Losers</h2>
-          <!-- Add your top gainers/losers widget here -->
-          <p>Stay informed about the stocks that are performing well and those that are experiencing losses.</p>
-        </div>
-
-        <!-- Watchlist Section -->
-        <div class="watchlistLiveMarket bg-white shadow p-6 rounded-lg">
-          <h2 class="text-2xl font-bold mb-4">Watchlist</h2>
-          <!-- Add your watchlist functionality here -->
-          <p>Create and maintain a personalized watchlist of stocks that you want to monitor closely.</p>
-        </div>
-
-        <!-- Portfolio Section -->
-        <div class="portfolioLiveMarket bg-white shadow p-6 rounded-lg">
-          <h2 class="text-2xl font-bold mb-4">Portfolio</h2>
-          <!-- Add your portfolio functionality here -->
-          <p>Track your investment holdings, view your portfolio performance, and make informed decisions about your investments.</p>
-        </div>
-
-        <!-- IPO Results -->
-        <div class="ipoLiveMarket bg-white shadow p-6 rounded-lg">
-          <h2 class="text-2xl font-bold mb-4">IPO Results</h2>
-          <p class="text-gray-600 mb-6">Stay updated with IPO results.</p>
-          <!-- Add your IPO results here -->
-          <p>Stay informed about the latest initial public offerings (IPOs) and their results.</p>
-        </div>
-
-        <!-- Broker Lists -->
-        <div class="brokerLiveMarket bg-white shadow p-6 rounded-lg">
-          <h2 class="text-2xl font-bold mb-4">Broker Lists</h2>
-          <!-- Add your broker list here -->
-          <p>Find information about authorized stockbrokers and their services, including contact details, address, and their TMS IDs.</p>
-        </div>
+  <section class="bottomFooter">
+    <div class="about grid grid-cols-1 lg:grid-cols-12 gap-4 bg-gray-100 rounded-t p-4">
+      <div class="aboutUs col-span-12 lg:col-span-8">
+        <h5 class="font-bold text-lg mb-2">About Us</h5>
+        <p>We are share Insight Nepal. We manage your portfolio. Go green, increase your revenue. For more info contact: 9800000000 or mail: support@shareinsightnepal.com</p>
+      </div>
+      <div class="socialMedia flex justify-center items-center gap-4 col-span-12 lg:col-span-4">
+        <a href="https://www.facebook.com/limitlessplaying" target="_blank"><i class="fa-brands fa-facebook fa-2xl"></i></a>
+        <a href="https://twitter.com/regmisuman_2000" target="_blank"><i class="fa-brands fa-twitter fa-2xl"></i></a>
+        <a href="https://www.youtube.com/c/limitlessplaying2000" target="_blank"><i class="fa-brands fa-youtube fa-2xl"></i></a>
+        <a href="https://www.instagram.com/regmisuman_2000" target="_blank"><i class="fa-brands fa-instagram fa-2xl"></i></a>
+        <a href="https://www.limitlessplaying.blogspot.com" target="_blank"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+      </div>
+    </div>
+    <div class="copyrightArea flex flex-col lg:flex-row justify-between items-center bg-gray-200 p-4 rounded-b">
+      <div class="text-center lg:text-left">
+        <p>Copyright &copy;2022-
+          <script>document.write(new Date().getFullYear())</script>
+          Share Insight Nepal All Rights Reserved</p>
+      </div>
+      <div class="flex gap-4">
+        <a href="/home">Home</a>
+        <a href="/policy">Privacy Policy</a>
+        <a href="/terms">Terms and Condition</a>
       </div>
     </div>
   </section>
-  <section class="bottomFooter row">
-    <div class="about pt-2 row rounded-top">
-        <div class="aboutUs col-md-12 col-lg-8 col-sm-12">
-            <h5>About Us</h5>
-            <p>We are share Insight Nepal. We manage your portfolio. Go green, increase your revenue. 
-                For More info Contact: 9800000000 or Mail us: support@shareinsightnepal.com</p>
-        </div>
-        <div class="socialMedia m-auto col-md-12 col-lg-4 col-sm-12 pb-1">
-                <a alt="facebook" class="facebook-btn ms-2" href="https://www.facebook.com/limitlessplaying" target="_blank"><i class="fa-brands fa-facebook fa-2xl"></i></a>
-                <a alt="twitter" class="twitter-btn ms-2" href="https://twitter.com/regmisuman_2000" target="_blank"><i class="fa-brands fa-twitter fa-2xl"></i></a>
-               <a alt="youtube" class="youtube-btn ms-2" href="https://www.youtube.com/c/limitlessplaying2000" target="_blank"><i class="fa-brands fa-youtube fa-2xl"></i></a>
-               <a alt="instagram" class="instagram-btn ms-2" href="https://www.instagram.com/regmisuman_2000" target="_blank"><i class="fa-brands fa-instagram fa-2xl"></i></a>
-                <a alt="linkedin" class="linkedin-btn ms-2" href="https://www.limitlessplaying.blogspot.com" target="_blank"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
-        </div>
-
-    </div>
-    <div class="copyrightArea navbar rounded-bottom d-flex gap-6 pt-2 row">
-        <div class="copyright col-md-12 col-lg-7 col-sm-12">
-        <p>Copyright &copy;2022-<script>document.write(new Date().getFullYear())</script> Share Insight Nepal All Rights Reserved</p>
-        </div>
-        <div class="col-md-12 col-lg-4 d-flex col-sm-12 gap-4">
-            <a class="ps-2 pe-2" href="/home">Home</a>
-            <a class="ps-2 pe-2" href="/policy">Privacy Policy</a>
-            <a class="ps-2 pe-2" href="/terms">Terms and Condition</a>
-        </div>
-    </div>
-</section>
 </body>
 </html>
